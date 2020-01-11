3b2b5d684d3227763cdc34b1460edef0b056fdfd
gabrielleberanger <gabrielle.beranger@gmail.com>
2020-01-11 16:26:44 +0100

    Improved security level generator

--- functions.py
+++ functions.py
@@ -4,9 +4,8 @@ def get_password_length():
     
     """Function asking the user to input the desired password length."""
     
-    check = False
-    while check == False:
-        password_length = input("Please enter the desired password length (from 1 to 20):")
+    while True:
+        password_length = input("\nStep 1 - Please enter the desired password length (from 1 to 20):")
         try:
             password_length = int(password_length)
         except:
@@ -15,66 +14,86 @@ def get_password_length():
         if password_length<=0 or password_length>20:
             print("Your input should be an integer between 1 and 20 (included).\n") 
             continue
-        check = True        
+        else:
+            break
     return password_length
    
-
 def get_password_criteria():
     
     """Function collecting user password criteria."""
     
     user_choices = dict()
     type_list = ["Uppercase","Lowercase","Numbers","Symbols"]
-    print("Please select password character types:")
+    print("Step 2 - Please select password character types:")
     for asked_type in type_list:
-        check = False
-        while check == False:
+        while True:
             chosen_type = input("- {} (y/n):".format(asked_type))
             if (chosen_type.lower() != "y") and (chosen_type.lower() != "n"):
                 print("Please enter 'y' for 'yes', or 'n' for 'no'.")
             elif chosen_type.lower() == "y":
                 user_choices[asked_type]=True
-                check = True
+                break
             else:
                 user_choices[asked_type]=False
-                check = True
+                break
     return user_choices
 
-
-def get_security_level(password_length,user_choices):
+def get_characters_range(user_choices):
     
-    """Function calculating the security level (WEAK, AVERAGE or STRONG)
-    from the password length and selected character types."""
-    
-    total_score = password_length*sum(list(user_choices.values()))
-    if total_score < 30:
-        security_level = "WEAK"
-    elif total_score < 40:
-        security_level = "AVERAGE"
-    else:
-        security_level = "STRONG"
-    return security_level
-
-
-def generate_password(password_length, user_choices):
-    
-    """Function generating a random password based on the criteria selected by the user."""
+    """Function generating the characters range from which the password will be drawn."""
     
     import string
-    import random
-    
-    #Generating the characters range from which the password will be drawn
     characters_dict =\
-    {"Lowercase" :[letter for letter in string.ascii_lowercase],\
-     "Uppercase" :[letter for letter in string.ascii_uppercase],\
-     "Numbers":[digit for digit in string.digits],\
-     "Symbols":[punctuation for punctuation in string.punctuation]}
+        {"Lowercase" :[letter for letter in string.ascii_lowercase],\
+        "Uppercase" :[letter for letter in string.ascii_uppercase],\
+        "Numbers":[digit for digit in string.digits],\
+        "Symbols":[punctuation for punctuation in string.punctuation]}
     characters_range = list()
     for key in characters_dict:
         if user_choices[key]==True:
             characters_range+=characters_dict[key]
+    return characters_range
+
+def generate_password(password_length, characters_range, user_choices):
     
-    #Generating random password
-    password = "".join(random.choices(characters_range, k=password_length))
+    """Function generating a random password based on the criteria selected by the user."""
     
+    import string
+    import secrets
+    while True:
+        password = "".join(secrets.choice(characters_range) for i in range(password_length))
+        #The secrets module is used for generating cryptographically strong random numbers.
+        if sum(character.isupper() for character in password) >= user_choices["Uppercase"]\
+            and sum(character.islower() for character in password) >= user_choices["Lowercase"]\
+            and sum(character.isdigit() for character in password) >= user_choices["Numbers"]\
+            and sum(character in string.punctuation for character in password) >= user_choices["Symbols"]:
+            break
     return password
+
+def get_password_strength(password_length,characters_range):
+    
+    """Function calculating password strength from the password length and the selected characters range."""
+    
+    from math import log
+    
+    max_attempts = len(characters_range)**password_length
+    
+    #Identify security level
+    entropy = log(max_attempts,2)
+    
+    if entropy < 50:
+        security_level = "VERY WEAK"
+    elif entropy < 60:
+        security_level = "WEAK"
+    elif entropy < 70:
+        security_level = "FAIR"
+    elif entropy < 80:
+        security_level = "STRONG"
+    else:
+        security_level = "VERY STRONG"
+    
+    #Identify the average time needed to guess the password
+    attempts_per_sec = 10**10
+    guess_time_sec = max_attempts/(attempts_per_sec*2)
+
+    return security_level, guess_time_sec
--- password-generator.py
+++ password-generator.py
@@ -3,27 +3,30 @@
 print("***WELCOME TO OUR PASSWORD GENERATOR!***\n")
 
 import time
-continue_generator = True
+from functions import *
 
-while continue_generator == True:
+while True:
 
     password_length = get_password_length()
     user_choices = get_password_criteria()
-
-    print("Given the chosen criteria, the security level of your password will be: {}\n"\
-          .format(get_security_level(password_length,user_choices)))
     
     for i in range(3):
-        time.sleep(1)
         print("Generating password...")
+        time.sleep(0.5)
+        
+    characters_range = get_characters_range(user_choices)
+    password = generate_password(password_length, characters_range, user_choices)
     
-    password = generate_password(password_length, user_choices)
     print("\nYour password is : {}".format(password))
+    
+    security_level, guess_time_sec = get_password_strength(password_length,characters_range)
+    print("Security level: {}".format(security_level))
+    print("A brute-force attack could guess your password in {} seconds.\n".format(guess_time_sec))
 
     user_input = input("Would you like to generate another password? (y/n)")
     if (user_input.lower() != "y") and (user_input.lower() != "n"):
         print("Please enter 'y' for 'yes', or 'n' for 'no'.")
     elif user_input.lower() == "n":
-        continue_generator = False
+        break
 
 print("Thank you for using our password generator. See you soon!")
